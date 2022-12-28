ee7a3984da99a3b44b90419b1c60255412690d51
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2022-12-28 15:05:50 +0600

    Update [1941] Check if All Characters Have Equal Number of Occurrences.java

--- [1941] Check if All Characters Have Equal Number of Occurrences.java
+++ [1941] Check if All Characters Have Equal Number of Occurrences.java
@@ -1,3 +1,5 @@
+//Using TreeMap
+
 class Solution {
     public boolean areOccurrencesEqual(String s) {
         int[] a=new int[26];
@@ -12,5 +14,30 @@ class Solution {
         }
         return true;
         
+    }
+}
+
+
+
+//Using PriorityQueue
+class Solution {
+    public int minStoneSum(int[] p, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((x,y)->y-x);
+        int out=0;
+        for(int i:p){
+            pq.add(i);
+            out+=i;
+        }
+            
+
+        while(k--!=0){
+            int t=pq.peek();
+            out-=t/2;
+            pq.remove(t);
+            pq.add(t-t/2);
+        }
+        
+
+        return out;        
     }
 }
