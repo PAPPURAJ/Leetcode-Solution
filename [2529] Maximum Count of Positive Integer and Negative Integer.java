ffaf12e2f093b55b9db481ca30d52f14354c8d23
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2023-01-08 18:24:38 +0600

    Create [2529] Maximum Count of Positive Integer and Negative Integer.java

---
+++ [2529] Maximum Count of Positive Integer and Negative Integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int p=0,n=0;
+        
+        for(int i:nums)
+            if(i<0)
+                n++;
+            else if(i>0)
+                p++;
+        
+        return Math.max(p,n);
+  
+    }
+}
