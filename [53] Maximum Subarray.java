6f035557921ac10c047abac3e3c56a31bbab1cd5
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2023-01-11 21:03:10 +0600

    Create [53] Maximum Subarray.java

---
+++ [53] Maximum Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum=Integer.MIN_VALUE,curSum=0;
+
+        for(int i:nums){
+            curSum+=i;
+            maxSum=Math.max(maxSum, curSum);
+            if(curSum<0)
+                curSum=0;    
+        }
+        return maxSum;    
+    }
+}
