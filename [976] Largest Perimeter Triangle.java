56b8d79ca6dcd25d640c3f25e6b65f33444c3708
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2023-02-18 15:33:20 +0600

    Create [976] Largest Perimeter Triangle.java

---
+++ [976] Largest Perimeter Triangle.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int largestPerimeter(int[] n) {
+        Arrays.sort(n);
+        for(int i=n.length-1;i>=2;i--)
+            if(n[i]+n[i-1]>n[i-2] && n[i]+n[i-2]>n[i-1] && n[i-2]+n[i-1]>n[i])
+                return n[i]+n[i-1]+n[i-2];    
+        return 0;    
+    }
+}
